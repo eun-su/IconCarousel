4af3c30249871319151d0ff31e151f3cb4f9dfac
eunsu <eunosuo@gmail.com>
2025-04-07 00:38:24 +0900

    20250407 start

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+// import IconCarousel from './components/IconCarousel';
+import VanillaCarousel from './components/VanillaCarousel';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>아이콘 캐러셀</h1>
+      {/* <IconCarousel /> */}
+      <VanillaCarousel />
+    </div>
+  );
+}
+
+export default App;
